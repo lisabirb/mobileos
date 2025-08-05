38e78996ea1b947fdd81388b7138f490e8b0e75a
lisabirb <93887296+lisabirb@users.noreply.github.com>
2025-08-05 06:40:08 -0600

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,24 +2,28 @@
 
 set -ouex pipefail
 
-### Install packages
+curl -fsSL https://repo.librewolf.net/librewolf.repo > /etc/yum.repos.d/librewolf.repo
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
 dnf5 -y group install kde-mobile
 dnf5 -y group install kde-mobile-apps
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+COPR_REPOS=(
+ublue-os/packages
+secureblue/bubblejail
+)
 
-#### Example for enabling a System Unit File
+PACKAGES=(
+librewolf
+ublue-brew
+neovim
+bubblejail
+fish
+)
+
+for i in ${COPR_REPOS[@]}; do
+  dnf -y copr enable $i
+done
+
+dnf5 -y install ${PACKAGES[@]}
 
 systemctl enable podman.socket
